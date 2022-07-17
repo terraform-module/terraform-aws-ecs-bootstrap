bd59ecb90d7974a23deacc2e5178ef10cc44ad14
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2022-07-17 08:10:34 +0100

    added cloudwatch explanation e.g. how to setup

--- telemetry.tf
+++ telemetry.tf
@@ -1,3 +1,15 @@
+# You can set a simple string and ECS will create the CloudWatch log group for you
+# or you can create the resource yourself as shown here to better manage retetion, tagging, etc.
+# Embedding it into the module is not trivial and therefore it is externalized
+# Input parameter
+# container_definitions ... {
+#   logDriver = "awslogs"
+#   options   = {
+#     awslogs-group        = "/ecs/NAME-ENV-task"
+#     awslogs-region       = "REGION"
+#     awslogs-stream-prefix = "NAME"
+#   }
+# }
 resource "aws_cloudwatch_log_group" "this" {
 
   for_each = { for k, v in var.service.container_definitions : k => v.logConfiguration
